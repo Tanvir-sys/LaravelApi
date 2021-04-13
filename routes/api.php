cc8abadddcf10789e77b7fbbafd015b1300c470e
https://github.com/Tanvir-sys/profile_.git <163hossaintanvir@gmail.com>
2021-04-14 01:53:47 +0600

    file Upload

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DemoApi;
+use App\Http\controllers\FileController;
 
 
 /*
@@ -21,4 +22,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
     Route::get("data",[DemoApi::class,'getData']);
-    
+    Route::get("data",[DemoApi::class,'data']);
+    Route::post("file",[FileController::class,'upload']);
+
